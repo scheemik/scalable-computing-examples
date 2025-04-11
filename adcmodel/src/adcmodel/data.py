088d34c2ee0a99bb887ea49f05f3fd2fa16cfd5a
scheemik <mikhail.schee@alumni.utoronto.ca>
2025-04-11 15:17:53 -0700

    Added adcmodel directory

---
+++ adcmodel/src/adcmodel/data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import urllib.request
+import hashlib
+import os
+
+
+def load_adc_csv(identifier=None):
+    """
+    Load and cache a CSV data file from the Arctic Data Center as a pandas.DataFrame
+    """
+    if identifier is None:
+        return pd.DataFrame()
+    hashval = hashlib.sha256(bytes(identifier, 'utf-8')).hexdigest()
+    path = "data/" + hashval
+    if not os.path.exists("data/"):
+        os.mkdir("data/")
+    if not os.path.exists(path):
+        service = "https://arcticdata.io/metacat/d1/mn/v2/object/"
+        url = service + identifier
+        msg = urllib.request.urlretrieve(url, path)
+    df = pd.read_csv(path)
+    return df
